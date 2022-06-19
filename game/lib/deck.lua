633c613d76b13f042846b50f6e7d610f99023a13
Shen Sat <shensat@shens-mbp.home>
2022-06-19 13:33:07 +0100

    Change cards to match cards found in real Orchard game
    As per comment in thread: https://www.lexaloffle.com/bbs/?pid=112534#p
    
    Players were finding game too easy.
    blixten wrote some code to make cards in game match cards in the real board game.
    I've tweaked this code and put it into game.

--- game/lib/deck.lua
+++ game/lib/deck.lua
@@ -1,29 +1,46 @@
-card_one = { copy_table(lemon), copy_table(berry), copy_table(apple), copy_table(apple), copy_table(lemon), copy_table(berry) }
-card_two = { copy_table(lemon), copy_table(lemon), copy_table(berry), copy_table(apple), copy_table(apple), copy_table(berry) }
-card_three = { copy_table(lemon), copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(apple), copy_table(berry) }
-card_four = { copy_table(berry), copy_table(berry), copy_table(apple), copy_table(lemon), copy_table(lemon), copy_table(apple) }
-card_five = { copy_table(berry), copy_table(apple), copy_table(lemon), copy_table(lemon), copy_table(apple), copy_table(berry) }
-card_six = { copy_table(lemon), copy_table(berry), copy_table(apple), copy_table(apple), copy_table(lemon), copy_table(berry) }
-card_seven = { copy_table(lemon), copy_table(lemon), copy_table(berry), copy_table(apple), copy_table(apple), copy_table(berry) }
-card_eight = { copy_table(lemon), copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(apple), copy_table(berry) }
-card_nine = { copy_table(apple), copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(lemon), copy_table(apple) }
-
-starting_cards = { card_one, card_two, card_three, card_four, card_five, card_six, card_seven, card_eight, card_nine }
+starting_cards = {{ -- Nine two-sided cards
+    {copy_table(berry), copy_table(apple), copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(lemon)}, -- 1
+    {copy_table(apple), copy_table(berry), copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(lemon)}  -- 2
+  }, {
+    {copy_table(berry), copy_table(apple), copy_table(lemon), copy_table(lemon), copy_table(apple), copy_table(berry)}, -- 3
+    {copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(lemon), copy_table(apple), copy_table(berry)}  -- 4
+  }, {
+    {copy_table(apple), copy_table(lemon), copy_table(berry), copy_table(berry), copy_table(lemon), copy_table(apple)}, -- 5
+    {copy_table(lemon), copy_table(apple), copy_table(berry), copy_table(berry), copy_table(lemon), copy_table(apple)}  -- 6
+  }, {
+    {copy_table(lemon), copy_table(apple), copy_table(lemon), copy_table(berry), copy_table(apple), copy_table(berry)}, -- 7
+    {copy_table(lemon), copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(apple), copy_table(berry)}  -- 8
+  }, {
+    {copy_table(berry), copy_table(lemon), copy_table(berry), copy_table(apple), copy_table(lemon), copy_table(apple)}, -- 9
+    {copy_table(berry), copy_table(lemon), copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(apple)}  -- 10
+  }, {
+    {copy_table(berry), copy_table(berry), copy_table(apple), copy_table(lemon), copy_table(lemon), copy_table(apple)}, -- 11
+    {copy_table(berry), copy_table(berry), copy_table(apple), copy_table(lemon), copy_table(apple), copy_table(lemon)}  -- 12
+  }, {
+    {copy_table(apple), copy_table(berry), copy_table(apple), copy_table(lemon), copy_table(berry), copy_table(lemon)}, -- 13
+    {copy_table(apple), copy_table(berry), copy_table(lemon), copy_table(apple), copy_table(berry), copy_table(lemon)}  -- 14
+  }, {
+    {copy_table(lemon), copy_table(berry), copy_table(apple), copy_table(apple), copy_table(berry), copy_table(lemon)}, -- 15
+    {copy_table(berry), copy_table(lemon), copy_table(apple), copy_table(apple), copy_table(berry), copy_table(lemon)}  -- 16
+  }, {
+    {copy_table(apple), copy_table(apple), copy_table(lemon), copy_table(berry), copy_table(berry), copy_table(lemon)}, -- 17
+    {copy_table(apple), copy_table(apple), copy_table(lemon), copy_table(berry), copy_table(lemon), copy_table(berry)}  -- 18
+  }
+}
 
 shuffled_cards = {}
 no_of_starting_cards = #starting_cards
 
-for c=1,no_of_starting_cards do
-  local i = flr(rnd(#starting_cards)) + 1
-  local card = starting_cards[i]
-  del(starting_cards,card)
-  add(shuffled_cards,card)
+for _i=1, no_of_starting_cards  do
+	local card = rnd(starting_cards) -- Get a random card
+	local side = rnd(card)           -- Select random side of that card
+	del(starting_cards, card)
+	add(shuffled_cards, side)
 end
-
 
 deck = {
 	cards = shuffled_cards,
-	count_after_first_card_placement = no_of_starting_cards - 1,
+	count_after_first_card_placement = #shuffled_cards - 1,
 	count = function(self)
 		return #self.cards
 	end
